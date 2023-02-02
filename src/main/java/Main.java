5f53698ff733b3d2002eefa4fd683ae4468d3c48
”Aliaksandr <moroz.ay@esphere.ru>
2023-02-02 12:31:00 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+import service.QuestionService;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        QuestionService.start();
+    }
+}
---
+++ src/main/java/entity/Question.java
@@ -0,0 +1,42 @@
+package entity;
+
+public class Question {
+
+    private String question;
+    private String answer;
+    private String correctAnswer;
+
+    public Question(String question, String correctAnswer) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    @Override
+    public String toString() {
+        return question;
+    }
+}
